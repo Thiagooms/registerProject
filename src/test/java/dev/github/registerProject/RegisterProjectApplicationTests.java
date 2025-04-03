f919bc10878b4842887398f4f94fe0372696996d
Thiago <thiagopaulista50@gmail.com>
2025-04-02 22:17:32 -0300

    first commit

---
+++ src/test/java/dev/github/registerProject/RegisterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.github.registerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
