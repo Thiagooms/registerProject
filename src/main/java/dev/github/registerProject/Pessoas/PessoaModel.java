6f3099b13df4e17e751b2a4aa3a4f57c50c61a2d
Thiago <thiagopaulista50@gmail.com>
2025-04-05 17:46:28 -0300

    [feat]: Add Lombok

--- src/main/java/dev/github/registerProject/Pessoas/PessoaModel.java
+++ src/main/java/dev/github/registerProject/Pessoas/PessoaModel.java
@@ -2,12 +2,19 @@ package dev.github.registerProject.Pessoas;
 
 import dev.github.registerProject.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 //Transform a class into an entity and Create a Table.
 //Issue #2 ok
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PessoaModel {
 
     @Id
@@ -20,47 +27,5 @@ public class PessoaModel {
     //Uma pessoa tem uma única tarefa, - @ManyToOne.
     @OneToMany(mappedBy = "pessoa")
     private TarefasModel tarefas;
-
-    public PessoaModel()  {
-
-    }
-
-    public PessoaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
 }
--- src/main/java/dev/github/registerProject/Tarefas/TarefasModel.java
+++ src/main/java/dev/github/registerProject/Tarefas/TarefasModel.java
@@ -2,9 +2,15 @@ package dev.github.registerProject.Tarefas;
 
 import dev.github.registerProject.Pessoas.PessoaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_tarefas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TarefasModel {
 
     @Id
@@ -17,39 +23,5 @@ public class TarefasModel {
     @ManyToOne
     @JoinColumn(name = "tarefas_id") //Foreing Key OU Chave estrangeira.
     private PessoaModel pessoa;
-
-    public TarefasModel() {
-
-    }
-
-    public TarefasModel(Long id, String nomeTarefa, String status) {
-        this.id = id;
-        this.nomeTarefa = nomeTarefa;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeTarefa() {
-        return nomeTarefa;
-    }
-
-    public void setNomeTarefa(String nomeTarefa) {
-        this.nomeTarefa = nomeTarefa;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 
 }
