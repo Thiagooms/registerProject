96193014b03ad981f793ff2b16085a14b984a7f7
Thiago <thiagopaulista50@gmail.com>
2025-04-15 13:25:30 -0300

    feat: update controller with mock date

--- src/main/java/dev/github/registerProject/Pessoas/PessoaController.java
+++ src/main/java/dev/github/registerProject/Pessoas/PessoaController.java
@@ -1,8 +1,6 @@
 package dev.github.registerProject.Pessoas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,6 +11,39 @@ public class PessoaController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
+    //Adicionar Pessoa (CREATE)
+
+    @PostMapping("/criar")
+    public String criarPessoa() {
+        return "Pessoa Cadastrada no Sistema!";
+    }
+
+    //Mostrar Todas as Pessoas por ID (CREATE)
+
+    @GetMapping("/todos")
+    public String mostrarTodosCadastrados(){
+        return "Pessoa Cadastrada";
+    }
+
+    //Mostrar Pessoas por ID  (READ)
+
+    @GetMapping("/todosID")
+    public String mostrarTodosCadastradosPorId(){
+        return "Pessoa Cadastrada por ID";
+    }
+
+    //Alterar Dados de uma Pessoa (UPDATE)
+
+    @PutMapping("/alterarID")
+    public String alterarPessoaPorId(){
+        return "Alterar Pessoa por ID";
+    }
+
+    //Deletar Pessoa (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarPessoaPorId(){
+        return "Pessoa Deletada por ID";
+    }
 
 
 }
