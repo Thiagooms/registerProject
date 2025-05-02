b92eaa7b55436881e0da37157718aeccfcfe5116
Thiago <thiagopaulista50@gmail.com>
2025-05-02 16:12:29 -0300

    feat: Create controller routes with mocks

--- src/main/java/dev/github/registerProject/Tarefas/TarefasController.java
+++ src/main/java/dev/github/registerProject/Tarefas/TarefasController.java
@@ -2,9 +2,7 @@ package dev.github.registerProject.Tarefas;
 
 //localhost:8080
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -14,11 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("missoes")
 public class TarefasController {
 
+    // GET -- Mandar uma requisição para mostrar missões
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missões listadas com sucesso";
+    }
+
+    // POST -- Mandar uma requisição para criar missões
     //localhost:8080/missoes/criar
     @PostMapping("/criar")
     public String criarTarefa() {
         return "Missão criada com sucesso!";
     }
 
+    // PUT -- Mandar uma requisição para alterar missões
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso";
+    }
+
+    // DELETE -- Mandar uma requisição para deletar missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso";
+    }
 
 }
